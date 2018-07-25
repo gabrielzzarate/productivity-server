e8e8e5d56d460053b917db6d0557431cb095351c
Gabriel Zarate <gabrielzarate@Gabriels-MBP.firststreet.local>
2018-07-25 11:28:42 -0400

    update connection to mongo session

--- server.js
+++ server.js
@@ -3,22 +3,44 @@ import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 import { graphql } from 'graphql';
 import { introspectionQuery } from 'graphql/utilities';
+import session from 'koa-session-store';
+import mongoStore from 'koa-session-mongo';
+import passport from 'passport';
+//import passportConfig from './services/auth';
 import graphqlHTTP from 'koa-graphql';
 import graphqlSchema from './src/data/schema';
 import fs from 'fs';
 import keys from './config/keys';
 import { initSeed } from './src/data/seeders/task.seeder';
 
-console.log(initSeed);
-
 const app = new Koa();
 const PORT = process.env.PORT || 3000;
 const schema = graphqlSchema();
+const MONGO_URI = 'mongodb://localhost:27017';
 const jsonSchema = graphql(schema, introspectionQuery);
+
+mongoose.connect(MONGO_URI);
+mongoose.connection
+  .once('open', () => console.log('Connected to MongoDB'))
+  .on('error', error => console.log('Error connecting to MongoDB', error));
+
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'aaabadfabc',
+  store: mongoStore.create({
+    db: 'mongo',
+    url: MONGO_URI,
+    autoReconnect: true
+  })
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(graphqlHTTP((ctx) => ({
   url: '/graphql',
-  schema: schema,
+  schema,
   graphiql: true
 })));
 
@@ -28,9 +50,6 @@ fs.writeFile('src/data/schema.json', JSON.stringify(jsonSchema, null, 2), err =>
   console.log('schema.json created successfully.');
 });
 
-mongoose.connect('mongodb://localhost:27017');
-
 app.listen(PORT, () => {
-  initSeed();
   console.info(`===> Listening on porty ${PORT}.`, PORT);
 });
